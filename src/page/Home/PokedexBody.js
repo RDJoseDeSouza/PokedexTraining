de9134ac20fa074d5d2263df18628f9b63007372
RDJoseDeSouza <jose441232@gmail.com>
2021-12-13 17:08:55 -0300

    up

--- src/page/Home/PokedexBody.js
+++ src/page/Home/PokedexBody.js
@@ -4,19 +4,19 @@ function PokedexBody(props){
   const pokemon = props.pokemon || []
   function renderImg(id){
     if(id<10){
-      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${id}.png`}/>
+      return <img className="card-image " src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${id}.png`}/>
     }else if(id<100){
-      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${id}.png`}/>
+      return <img className="card-image " src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${id}.png`}/>
 
     }else{
-      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`}/>
+      return <img className="card-image " src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`}/>
     }
 
   }
 
   function listPoke(){
     return pokemon.map(function(eachOne){
-      return<li key={eachOne.id} className="card"> 
+      return<li key={eachOne.id} className={"card "+ eachOne.types[0].type.name}> 
           {renderImg(eachOne.id)}
           <h2 className="card-title">{eachOne.id}. {eachOne.name}</h2>
           <p className="card-subtitle">{eachOne.types.map(typeEach=> typeEach.type.name).join(" | ")}</p>
