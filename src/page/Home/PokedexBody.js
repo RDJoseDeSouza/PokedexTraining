35a3de177b794efbcc49172472867c146d7add87
RDJoseDeSouza <jose441232@gmail.com>
2021-12-13 16:59:31 -0300

    Pokedex show

--- src/page/Home/PokedexBody.js
+++ src/page/Home/PokedexBody.js
@@ -2,14 +2,30 @@ import React from "react"
 import './style.css'
 function PokedexBody(props){
   const pokemon = props.pokemon || []
+  function renderImg(id){
+    if(id<10){
+      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${id}.png`}/>
+    }else if(id<100){
+      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${id}.png`}/>
+
+    }else{
+      return <img className="card-image flying" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`}/>
+    }
+
+  }
+
   function listPoke(){
     return pokemon.map(function(eachOne){
       return<li key={eachOne.id} className="card"> 
-        {eachOne.name}
-      </li>
+          {renderImg(eachOne.id)}
+          <h2 className="card-title">{eachOne.id}. {eachOne.name}</h2>
+          <p className="card-subtitle">{eachOne.types.map(typeEach=> typeEach.type.name).join(" | ")}</p>
+         </li>
     })
 
   }
+
+
   return <div className="container">
     <h1>Pokedex</h1>
     <ul data-js="pokedex" className="pokedex">
